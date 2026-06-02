9b6deb472624f3c555679c7079969ed1c1300f4b
Jaime Rodriguez <jaimedrodriguezf@gmail.com>
2026-06-01 20:30:26 -0500

    fix: use ClientThemeProvider to prevent React 19 async Client Component error in ThemeProvider

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,11 +1,31 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "@wrksz/themes/next"
+import { ClientThemeProvider } from "@wrksz/themes/client"
+
+export interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: "class" | "data-theme"
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
 
 export function ThemeProvider({
   children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  attribute = "class",
+  defaultTheme = "system",
+  enableSystem = true,
+  disableTransitionOnChange = false,
+}: ThemeProviderProps) {
+  return (
+    <ClientThemeProvider
+      attribute={attribute}
+      defaultTheme={defaultTheme}
+      enableSystem={enableSystem}
+      disableTransitionOnChange={disableTransitionOnChange}
+    >
+      {children}
+    </ClientThemeProvider>
+  )
 }
